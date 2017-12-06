9c981a8bffdf340ddcf19837ae066b5e0aab7d9b
green-dalii <654534332@qq.com>
2017-12-06 21:09:13 +0800

    modify index route & add 404 route

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,11 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Greener!"
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     application.run()

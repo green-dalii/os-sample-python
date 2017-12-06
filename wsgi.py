29d76b3be9a9f268125835d0bf46ae86a2df280d
green-dalii <654534332@qq.com>
2017-12-06 21:47:22 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@pplication.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 

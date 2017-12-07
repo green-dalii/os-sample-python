6f31e694ce315a444e40efaefd33dcb11e9b2e8a
green-dalii <654534332@qq.com>
2017-12-07 19:11:38 +0800

    add WeiXin API

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,10 @@ application = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@application.route("/wxapp")
+def wxapp():
+    return 'Hello form OpenShift!'
+
 @application.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
